6c4f21cc212f1cc713916ba1814cfbbd33d6e858
luckymo-on <126798657+luckymo-on@users.noreply.github.com>
2025-04-02 15:28:31 +0530

    webhook demo

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello{
 	public static void main(String args[]){
 		System.out.println("hello World ...!!!");
 		System.out.println("changed!!!");
+		System.out.println("webhook activated");
 
 
 
